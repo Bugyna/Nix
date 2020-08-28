56385df2cb1f4b33ba788531deac7d679907190f
M-Bugy <matejbugy@gmail.com>
2020-08-28 20:48:01 +0200

    fast as fuck boiii

--- highlighter.py
+++ highlighter.py
@@ -14,9 +14,9 @@ class highlighter():
 			]
 
 		self.C_keywords = [
-		'auto', 'break', 'case', 'char', 'const', 'continue', 'default', 'do', 'double', 'else', 'enum',
-		 'extern', 'float', 'for', 'goto', 'if', 'int', 'long', 'register', 'return', 'short', 'signed', 'sizeof',
-		  'static', 'struct', 'switch', 'typedef', 'union', 'unsigned', 'void', 'volatile', 'while',
+			'auto', 'break', 'case', 'char', 'const', 'continue', 'default', 'do', 'double', 'else', 'enum',
+		 	'extern', 'float', 'for', 'goto', 'if', 'int', 'long', 'register', 'return', 'short', 'signed', 'sizeof',
+		  	'static', 'struct', 'switch', 'typedef', 'union', 'unsigned', 'void', 'volatile', 'while',
    		]
 		
 		self.keywords = []
@@ -24,152 +24,201 @@ class highlighter():
 		if (lang == "c"):
 			self.keywords = self.C_keywords
 			self.highlight = self.C_highlight
+			self.commment_regex = re.compile(r"[//]")
 		elif (lang == "py"):
 			self.keywords = self.Py_keywords
 			self.highlight = self.python_highlight
+			self.commment_regex = re.compile(r"[\#]")
 
 		self.other_chars = ["$","#","@","&","|","^","_","\\",r"\\",r"\[\]",r"[\\]"]
-		# all_key = [keywords, other_chars]
-		# all_key = list(chain.from_iterable(all_key))
+
 		self.txt = text
 		self.theme = theme
 
 		self.countingQuomarks = False
 		self.Quomark_count = 0
 
-
-
-	# def sumn(self, st, index):
-	# 	index1 = 0
-	# 	for current_char in st:
-	# 		if (re.match(r"[a-zA-Z]", current_char)):
-
+		self.quote_regex = re.compile(r"[\"\']")
+		self.abc_regex = re.compile(r"[a-zA-Z]")
+		self.separator_regex = re.compile(r"[\s\.\,\(\)]")
+		self.num_regex = re.compile(r"[0-9]")
+		self.special_char_regex = re.compile(r"[\{\}\&\^\|]")
+		self.equal_regex = re.compile(r"[\=]")
+		self.operator_regex = re.compile(r"[\%\+\-\*\/]")
+		self.string_special_char_regex = re.compile(r"[\\{\}]")
+		
+	def get_line_lenght(self, line_no):
+		for i, char in enumerate(self.txt.get(float(line_no), "end"), 0):
+			if (re.match(r"\n", char)):
+				return f"{line_no}.{i}"
 
 	def python_highlight(self, line_no ,line=None):
 		if line == None:
-			line = self.txt.get(float(line_no), "end")
+			line = self.txt.get(float(line_no), self.get_line_lenght(line_no))
+			# print(line)
 
 		last_separator_index = 0
 		last_separator = f"{line_no}.{last_separator_index}"
-		pattern = ""
-		self.txt.tag_remove(self.theme["quotes"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["functions"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["keywords"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["numbers"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["special_chars"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["comments"], last_separator, f"{line_no}.{len(line)}")
+		line_end_index = f"{line_no}.{len(line)}"
+		self.pattern = ""
+		self.txt.tag_remove(self.theme["quotes"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["functions"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["keywords"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["numbers"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["special_chars"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["comments"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["operators"], last_separator, line_end_index)
 		
 		for i, current_char in enumerate(line, 0):
 			index = f"{line_no}.{i}"
 			# if (i == 0):
 			# 	self.countingQuomarks = False
 			
-			if (re.match(r"[\"\']", current_char)):
+			if (self.quote_regex.match(current_char)):
 				self.txt.tag_add(self.theme["quotes"], index)
-				self.countingQuomarks = not self.countingQuomarks
-				pattern = ""
-			
+				if (self.countingQuomarks):
+					self.countingQuomarks = False
+				else:
+					self.countingQuomarks = True
+				# self.countingQuomarks = not self.countingQuomarks
+
 			elif (self.countingQuomarks):
+				self.txt.tag_add(self.theme["quotes"], index)
+				# last_separator_index = i+2
+				# last_separator = f"{line_no}.{last_separator_index}"
+				# i = last_separator_index
+				# if (self.string_special_char_regex.match(current_char)):
+				# 	self.txt.tag_add(self.theme["special_chars"], index, last_separator)	
+				# else:
+				# 	self.txt.tag_add(self.theme["quotes"], index)
 				continue
-
-			elif (re.match(r"[a-zA-Z]", current_char)):
-				pattern += current_char
 			
-			elif (re.match(r"[\s\.\,\:\(]", current_char)):
-				if (pattern in self.keywords):
-					self.txt.tag_add(self.theme["keywords"], last_separator, index)
-
-				elif (re.match(r"\(", current_char)):
+			elif (self.abc_regex.match(current_char)):
+				self.pattern += current_char
+				continue
+			
+			elif (self.separator_regex.match(current_char)):
+				# print(self.pattern)
+				if (re.match(r"\(", current_char)):
 					self.txt.tag_add(self.theme["functions"], last_separator, index)
+				elif (self.pattern in self.keywords):
+					self.txt.tag_add(self.theme["keywords"], last_separator, index)
 					
 				last_separator_index = i+1
-				last_separator = f"{line_no}.{i+1}"
-				pattern = ""
+				last_separator = f"{line_no}.{last_separator_index}"
+				self.pattern = ""
+				continue
 
-
-			elif (re.match(r"[0-9]", current_char)): #numbers
-				self.txt.tag_add(self.theme["numbers"], index) 
-				pattern = ""
-			
-			elif (re.match(r"[\-\+\*\/\%\^\&\|\=\[\]\{\}]", current_char)): #special chars[\[\]\{\}\-\+\*\/\%\^\&\(\)\|\=]
-				self.txt.tag_add(self.theme["special_chars"], index)
-				pattern = ""
-
-			elif (re.match(r"[\#]", current_char)): #comments
+			elif (self.commment_regex.match(current_char)): #comments
 				self.txt.tag_add(self.theme["comments"], index, f"{line_no}.{i+1000}")
 				break
 
+			elif (self.num_regex.match(current_char)): #numbers
+				if (self.pattern == ""):
+					self.txt.tag_add(self.theme["numbers"], index)
+					self.pattern = ""
+				else:
+					self.pattern += current_char
+				continue
+			
+			elif (self.operator_regex.match(current_char)):
+				self.txt.tag_add(self.theme["operators"], index)
+				self.pattern = ""
+				continue
+			
+			elif (self.special_char_regex.match(current_char)): #special chars[\[\]\{\}\-\+\*\/\%\^\&\(\)\|\=]
+				self.txt.tag_add(self.theme["special_chars"], index)
+				self.pattern = ""
+				continue
+				
+			elif (self.equal_regex.match(current_char)):
+				self.txt.tag_add(self.theme["operators"], index)
+				last_separator_index = i+1
+				last_separator = f"{line_no}.{last_separator_index}"
+				self.pattern = ""
 
 	def C_highlight(self, line_no, line=None):
 		if line == None:
 			line = self.txt.get(float(line_no), "end")
 
-
 		last_separator_index = 0
 		last_separator = f"{line_no}.{last_separator_index}"
-		pattern = ""
+		line_end_index = f"{line_no}.{len(line)}"
+		self.pattern = ""
 
-		self.txt.tag_remove(self.theme["quotes"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["functions"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["keywords"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["numbers"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["special_chars"], last_separator, f"{line_no}.{len(line)}")
-		self.txt.tag_remove(self.theme["comments"], last_separator, f"{line_no}.{len(line)}")
+		self.txt.tag_remove(self.theme["quotes"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["functions"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["keywords"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["numbers"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["special_chars"], last_separator, line_end_index)
+		self.txt.tag_remove(self.theme["comments"], last_separator, line_end_index)
 		
 		for i, current_char in enumerate(line, 0):
 			index = f"{line_no}.{i}"
 			if (i == 0):
 				self.countingQuomarks = False
 
-			# if (re.match(r'"', current_char)):
-			#     self.Quomark_count = len(re.findall(current_char, self.txt.get(line_no+".0", "end")))
-
-
-			#     if self.countingQuomarks and self.Quomark_count % 2 == 0:
-			#         self.countingQuomarks = False 
-			#     else:
-			#         self.countingQuomarks = True
-			# if (re.findall(r'"', self.txt.get(line_no+".0", "end")) % 2 == 0):
 
 			try:
-				if (re.match(r"//", current_char+line[i+1])): #comments
+				if (self.commment_regex.match(r"//", current_char+line[i+1])): #comments
 					self.txt.tag_add(self.theme["comments"], index, f"{line_no}.{i+1000}")
 					break
 
 			except Exception:
 				pass
 
-			if (re.match(r"[\"\']", current_char)):
+			if (self.quote_regex.match(current_char)):
 				self.txt.tag_add(self.theme["quotes"], index)
-				self.countingQuomarks = not self.countingQuomarks
-				pattern = ""
-			
+				if (self.countingQuomarks):
+					self.countingQuomarks = False
+				else:
+					self.countingQuomarks = True
+				# self.countingQuomarks = not self.countingQuomarks
+
 			elif (self.countingQuomarks):
+				self.txt.tag_add(self.theme["quotes"], index)
+				# last_separator_index = i+2
+				# last_separator = f"{line_no}.{last_separator_index}"
+				# i = last_separator_index
+				# if (self.string_special_char_regex.match(current_char)):
+				# 	self.txt.tag_add(self.theme["special_chars"], index, last_separator)	
+				# else:
+				# 	self.txt.tag_add(self.theme["quotes"], index)
 				continue
-
-			elif (re.match(r"[a-zA-Z]", current_char)):
-				pattern += current_char
 			
-			elif (re.match(r"[\s\.\,\:\(]", current_char)):
-				if (pattern in self.keywords):
-					self.txt.tag_add(self.theme["keywords"], last_separator, index)
-
-				elif (re.match(r"\(", current_char)):
+			elif (self.abc_regex.match(current_char)):
+				self.pattern += current_char
+				continue
+			
+			elif (self.separator_regex.match(current_char)):
+				# print(self.pattern)
+				if (re.match(r"\(", current_char)):
 					self.txt.tag_add(self.theme["functions"], last_separator, index)
+				elif (self.pattern in self.keywords):
+					self.txt.tag_add(self.theme["keywords"], last_separator, index)
 					
 				last_separator_index = i+1
-				last_separator = f"{line_no}.{i+1}"
-				pattern = ""
+				last_separator = f"{line_no}.{last_separator_index}"
+				self.pattern = ""
+				continue
 
+			elif (self.commment_regex.match(current_char)): #comments
+				self.txt.tag_add(self.theme["comments"], index, f"{line_no}.{i+1000}")
+				break
 
-			elif (re.match(r"[0-9]", current_char)): #numbers
-				self.txt.tag_add(self.theme["numbers"], index) 
-				pattern = ""
+			elif (self.num_regex.match(current_char)): #numbers
+				if (self.pattern == ""):
+					self.txt.tag_add(self.theme["numbers"], index)
+					self.pattern = ""
+				else:
+					self.pattern += current_char
+				continue
+
 			
-			elif (re.match(r"[\-\+\*\/\%\^\&\|\=\[\]\{\}]", current_char)): #special chars[\[\]\{\}\-\+\*\/\%\^\&\(\)\|\=]
+			elif (self.special_char_regex.match(current_char)): #special chars[\[\]\{\}\-\+\*\/\%\^\&\(\)\|\=]
 				self.txt.tag_add(self.theme["special_chars"], index)
-				pattern = ""
-
+				self.pattern = ""
+				continue
 
 	def unhighlight(self, line_no, line=None):
 		if line == None:
