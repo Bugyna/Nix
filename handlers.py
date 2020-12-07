082fc0ce2ee8fed0cd78bc86a2200d5a5a94b177
M-Bugy <matejbugy@gmail.com>
2020-12-07 22:51:25 +0100

    mostly bugfixing and general changes to make it easier to tinker around
    There's been a change to how text buffers are created and stored; It is now stored in both dictionary and a list which makes buffers ( and their buffer tabs ) easier to access ( no stupid functions to convert dictionary to list ).

--- handlers.py
+++ handlers.py
@@ -3,61 +3,96 @@ import time
 import tkinter
 import threading
 import subprocess
-from pygame import mixer
+try: from pygame import mixer
+except ImportError: pass
 
 from widgets import BUFFER_TAB, TEXT
 
 class file_handler(object):
 	""" File opening and closing yes"""
 	def __init__(self, parent):
-		self.supported_filetypes = ["TXT files", "*.txt *.py *.c *.cpp *.cc *.html *.htm *.css"]
+		self.supported_filetypes = ["TXT files", "*.txt *.py *.c *.cc *.cpp *.h *.hh *.hpp *.html *.htm *.css *.java *.class *.go *.sh *.diary"]
 		self.current_dir = os.getcwd()
 		self.current_file = None
 		self.current_file_name = None
-		self.current_buffer = ""
-		self.buffers = {}
-		self.buffer_tab_index = None
+		self.current_buffer = None
+
+		self.buffer_dict = {}
+		self.buffer_list = []
+
+		self.buffer_tab = None
+		self.buffer_tab_index = None # buffer tabs have the same index as text widgets
+		self.buffer_tab_list = []
+
 		self.parent = parent
 
 	def init(self):
 		self.current_buffer = "<~NONE>"
-		self.buffers[self.current_buffer] = TEXT(self.parent)
+		self.buffer_list.append(TEXT(self.parent, "<~NONE>"))
+		print(self.buffer_list[-1].buffer_index)
+		self.buffer_dict[self.current_buffer] = self.buffer_list[-1]
+
 
 	def rename_buffer(self, buffer_name: str, new_buffer_name: str):
-		for buffer in self.parent.buffer_tabs:
-			if (buffer_name == buffer.name): buffer.change_name(new_buffer_name); break
+		for buffer in self.buffer_tab_list:
+			if (buffer_name == buffer.full_name): buffer.change_name(new_buffer_name); break
 
-		self.buffers[new_buffer_name] = self.buffers.pop(buffer_name)
+		self.buffer_dict[new_buffer_name] = self.buffer_dict.pop(buffer_name)
 		self.current_buffer = new_buffer_name
 
 
 	def new_buffer(self, buffer_name: str):
-		try: self.buffers[buffer_name]; return # Checks for existing buffers
+		try: self.buffer_dict[buffer_name]; return # Checks for existing buffers
 		except KeyError: pass
-		self.parent.txt.place_forget()
-		self.current_file_name = self.current_buffer = buffer_name
-		self.buffers[buffer_name] = [TEXT(self.parent), BUFFER_TAB(buffer_name, self.parent)]
-		self.buffer_tab_index = self.buffers[self.current_buffer][1].index
-		self.parent.txt = self.buffers[buffer_name][0]
+
+		self.parent.hide_text_widget()
+
+		self.buffer_list.append([TEXT(self.parent, buffer_name), BUFFER_TAB(buffer_name, self.parent)])
+		self.buffer_dict[buffer_name] = self.buffer_list[-1]
+		self.parent.txt = self.buffer_list[-1][0]
+		self.buffer_tab = self.buffer_list[-1][1]
+		self.buffer_tab_list.append(self.buffer_list[-1][1])
+		self.buffer_tab_index = self.parent.txt.buffer_index # text widget's buffer_index is the same as their buffer tab's buffer_index
+
+
 		self.parent.theme_load()
-		self.parent.title(f"Nix: <{os.path.basename(self.current_buffer)}>")
+		self.current_file_name = self.current_buffer = buffer_name
+		self.current_dir = os.path.dirname(self.current_file_name)
+
+		self.parent.set_font_size()
+		self.parent.title(f"Nix: <{self.parent.txt.name}>")
 
 	def del_buffer(self, buffer_name: str=None):
-		self.buffers[buffer_name][1].place_forget()
-		self.buffers.pop(buffer_name)
+		buffer_index = self.buffer_dict[buffer_name][1].buffer_index
+		self.buffer_dict[buffer_name][0].place_forget()
+		self.buffer_dict[buffer_name][1].place_forget()
+		
+		self.buffer_list.pop(buffer_index)
+		self.buffer_dict.pop(buffer_name)
 
 		last_buffer_tab = None
-		for buffer in list(self.buffers.values())[1:]:
+		for buffer in self.buffer_list[1:]:
 			buffer[1].reposition(last_buffer_tab)
 			last_buffer_tab = buffer[1]
 
-	def load_buffer(self, buffer_name: str):
-		self.parent.txt.place_forget()
-		self.current_file_name = self.current_buffer = buffer_name; self.parent.highlighter.txt = self.parent.txt = self.buffers[buffer_name][0]
-		self.buffer_tab_index = self.buffers[self.current_buffer][1].index
+
+	def load_buffer(self, buffer_name: str = None, buffer_index: int = None):
+		if (self.parent.split_mode == 0): self.parent.hide_text_widget()
+		
+		if (not buffer_name and not buffer_index): print("error"); return -1
+		if (buffer_index and not buffer_name): buffer_name = self.buffer_list[buffer_index][1].name
+
+		self.current_file_name = self.current_buffer = buffer_name
+		self.parent.txt = self.buffer_dict[buffer_name][0]
+		self.buffer_tab = self.buffer_dict[buffer_name][1]
+		self.buffer_tab_index = self.parent.txt.buffer_index # text widget's buffer_index is the same as their buffer tab's buffer_index
+
 		self.current_dir = os.path.dirname(self.current_file_name)
-		self.parent.txt.place(x=0,y=40,relwidth=1, height=self.parent.winfo_height()-25, anchor="nw")
-		self.parent.title(f"Nix: <{os.path.basename(self.current_buffer)}>")
+		
+		self.parent.reposition_widgets()
+		self.parent.title(f"Nix: <{self.parent.txt.name}>")
+		self.parent.set_font_size()
+		self.parent.theme_load()
 
 	def del_file(self, filename:str=""):
 		if (not filename): filename=self.current_file_name
@@ -74,28 +109,32 @@ class file_handler(object):
 
 		self.current_file_name = name
 		self.current_file = open(self.current_file_name, "w+")
-		print(self.current_file.name)
 		self.new_buffer(self.current_file.name)
-		self.parent.txt.delete("1.0", "end")
-		# self.current_dir = os.path.dirname(self.current_file.name)
-		self.parent.title(f"Nix: <{os.path.basename(self.current_file.name)}>")
+
+		self.current_dir = os.path.dirname(self.current_file.name)
+		self.parent.title(f"Nix: <{self.parent.txt.name}>")
 		
-		self.parent.set_highlighter()
+		self.parent.txt.set_highlighter()
 
 	def save_file(self, arg = None):
 		""" saves current text into opened file """
 		self.buffer = str(self.parent.txt.get("1.0", "end-1c"))
 		
 		if (self.current_file_name):
+			size0 = os.path.getsize(self.current_file_name)
+
 			self.current_file = open(self.current_file_name, "w")
 			self.current_file.write(self.parent.txt.get("1.0", "end"))
-			
-			self.parent.set_highlighter()
-
 			self.current_file.close()
+
+			self.parent.txt.set_highlighter()
+			size1 = os.path.getsize(self.current_file_name)
+			
 			self.current_dir = os.path.dirname(self.current_file.name)
 			self.parent.title(f"Nix: <{os.path.basename(self.current_file_name)}>")
-			self.parent.command_O(f"total of {self.parent.get_line_count()} lines saved")
+			self.buffer_tab.change_name(extra_char="")
+			# self.parent.command_O(f"total of {self.parent.get_line_count()} lines saved")
+			self.parent.command_O(rf"saved {size1-size0}\{size1}\{self.parent.get_line_count()} new bytes to {os.path.basename(self.current_file_name)}")
 			
 		elif (not self.current_file_name):
 			self.new_file()
@@ -113,9 +152,9 @@ class file_handler(object):
 		else:
 			self.current_file_name = self.parent.filename.asksaveasfilename(initialdir=f'{self.current_dir}', title="Save file as", defaultextension=".txt" ,filetypes=(self.supported_filetypes, ("all files","*.*")))
 
-		os.rename(self.current_file.name, tmp)
 		self.current_file_name = tmp
 		self.rename_buffer(self.current_buffer, tmp)
+		os.rename(self.current_file.name, tmp)
 
 		self.current_dir = os.path.dirname(self.current_file.name)
 		self.parent.title(f"Nix: <{os.path.basename(self.current_file_name)}>")
@@ -128,39 +167,47 @@ class file_handler(object):
 
 		if (filename): #if the filename arguments is given: set the current filename to be the argument (pretty self explanatory)
 			if (os.path.dirname(filename)): self.current_file_name = f"{filename}"
-			else: self.current_file_name = f"{self.current_dir}/{filename}"
+			else: self.current_file_name = self.current_buffer = f"{self.current_dir}/{filename}"
 		
 		elif (filename == None): #if the filename argument is not provided open a file menu to provide a filename
 			try:
-				self.current_file_name = self.parent.filename.askopenfilename(initialdir=f"{self.current_dir}/", title="Select file", filetypes=(self.supported_filetypes, ("all files","*.*")))
+				self.current_file_name = self.current_buffer = self.parent.filename.askopenfilename(initialdir=f"{self.current_dir}/", title="Select file", filetypes=(self.supported_filetypes, ("all files","*.*")))
 			except TypeError: #throws an error when closing the menu without choosing anything
 				pass
 			
 		try:
 			self.current_file = open(self.current_file_name, "r+") #opens the file
-			self.parent.set_highlighter() #takes the file extension and passes it to the set_highlighter function to highlight the file accordingly
-		except Exception as e:
-			self.new_file(filename)
-			self.parent.command_O("new file")
+		except FileNotFoundError:
+			self.new_file(self.current_file_name)
 			return
 
-		self.new_buffer(self.current_file.name)
-		self.current_dir = os.path.dirname(self.current_file.name)
-		self.parent.title(f"Nix: <{os.path.basename(self.current_file.name)}>") #sets the title of the window to the current filename
-		self.parent.txt.delete("1.0", "end-1c") #deletes the buffer so there's not any extra text
 
 		t0 = time.time() # timer| gets current time in miliseconds
-		self.parent.txt.insert("1.0", self.current_file.read()) #puts all of the file's text in the text widget
-		self.parent.text_len = len(self.parent.txt.get("1.0", "end"))
+		self.new_buffer(self.current_file.name)
+
+		self.current_dir = os.path.dirname(self.current_file.name)
+		file_content = self.current_file.read()
+
+		file = open("."+os.path.basename(self.current_file_name)+".error_swp", "w+")
+		file.write(file_content)
+		file.close()
+
+		self.parent.txt.delete("1.0", "end-1c") #deletes the buffer so there's not any extra text
+		self.parent.txt.insert("1.0", file_content) #puts all of the file's text in the text widget
+		self.parent.txt.text_len = len(self.parent.txt.get("1.0", "end"))
 		self.parent.txt.mark_set(tkinter.INSERT, "1.0") #puts the cursor at the start of the file
 		self.parent.txt.see(tkinter.INSERT) #puts the cursor at the start of the file
+	
 		self.current_file.close() #closes current file
-		
+	
 		self.parent.highlight_chunk() #highlights the text in the text widget
 		t1 = time.time() # timer| gets current time in miliseconds
 		elapsed_time = round(t1-t0, 3) #elapsed time
 		print(t1-t0)
 		self.parent.command_O(f"total lines: {self.parent.get_line_count()};	loaded in: {elapsed_time} seconds") #puts the time it took to load and highlight the text in the command output widget
+		self.parent.title(f"Nix: <{self.parent.txt.name}>") #sets the title of the window to the current filename
+
+		del file_content
 
 class music_player:
 	def __init__(self, parent):
@@ -229,3 +276,4 @@ def video_record_stop(process):
 class launcher:
 	def __init__(self):
 		pass
+
